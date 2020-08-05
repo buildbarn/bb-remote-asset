4bd6f4f3315cf89a5f7da0bf7a9d785283f092a3
Josh Smith <josh.smith@codethink.co.uk>
2020-08-05 16:10:29 +0100

    Add caching fetch server with stubbed downloader

---
+++ pkg/fetch/unimplemented_fetcher.go
@@ -0,0 +1,27 @@
+package fetch
+
+import (
+	"context"
+	remoteasset "github.com/bazelbuild/remote-apis/build/bazel/remote/asset/v1"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type unimplementedFetcher struct {
+}
+
+// NewAssetFetchServer creates a blank Fetcher with both FetchBlob
+// and FetchDirectory unimplemented
+func NewUnimplementedFetcher() remoteasset.FetchServer {
+	return &unimplementedFetcher{
+	}
+}
+
+func (f *unimplementedFetcher) FetchBlob(ctx context.Context, req *remoteasset.FetchBlobRequest) (*remoteasset.FetchBlobResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "FetchBlob not implemented")
+}
+
+func (f *unimplementedFetcher) FetchDirectory(ctx context.Context, req *remoteasset.FetchDirectoryRequest) (*remoteasset.FetchDirectoryResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "FetchDirectory not implemented")
+}
