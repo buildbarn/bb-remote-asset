09b7dac7ddc0f9c36226c3b5c30670e8a42b1781
Thomas Coldrick <thomas.coldrick@codethink.co.uk>
2020-08-05 11:32:14 +0100

    Add abstractions for dealing with AssetReference
    Allows easier use of the AssetReference in the servers.

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/bazelbuild/remote-apis v0.0.0-20200730154707-006a399149dd
 	github.com/buildbarn/bb-storage v0.0.0-20200802055539-f0281e269c07
+	github.com/golang/protobuf v1.4.2
 	github.com/gorilla/mux v1.7.4
 	google.golang.org/grpc v1.31.0
 )
---
+++ pkg/storage/asset_reference.go
@@ -0,0 +1,35 @@
+package storage
+
+import (
+	"crypto/sha256"
+	"sort"
+
+	remoteasset "github.com/bazelbuild/remote-apis/build/bazel/remote/asset/v1"
+
+	"github.com/buildbarn/bb-asset-hub/pkg/proto/asset"
+	"github.com/buildbarn/bb-storage/pkg/digest"
+	"github.com/golang/protobuf/proto"
+)
+
+// NewAssetReference creates a new AssetReference from a URI and a list
+// of qualifiers. Mainly this is a wrapper to ensure the qualifiers get
+// sorted
+func NewAssetReference(uri string, qualifiers []*remoteasset.Qualifier) asset.AssetReference {
+	sortedQualifiers := qualifierContainer(qualifiers)
+	sort.Sort(sortedQualifiers)
+	return asset.AssetReference{Uri: uri, Qualifiers: sortedQualifiers.toArray()}
+}
+
+// AssetReferenceToDigest converts an AssetReference proto into a Digest
+// for storage
+func AssetReferenceToDigest(ar *asset.AssetReference, instance digest.InstanceName) (digest.Digest, error) {
+	wireFormat, err := proto.Marshal(ar)
+	if err != nil {
+		return digest.Digest{}, err
+	}
+
+	hash := sha256.Sum256(wireFormat)
+	sizeBytes := int64(len(wireFormat))
+
+	return instance.NewDigest(string(hash[:]), sizeBytes)
+}
