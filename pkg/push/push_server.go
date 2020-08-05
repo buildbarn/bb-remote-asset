c91a1f61dffae1e70dba26bd3a8d9800a581f3ca
Thomas Coldrick <thomas.coldrick@codethink.co.uk>
2020-08-05 11:33:08 +0100

    push: Implement basic push server

--- pkg/push/push_server.go
+++ pkg/push/push_server.go
@@ -4,9 +4,11 @@ import (
 	"context"
 
 	remoteasset "github.com/bazelbuild/remote-apis/build/bazel/remote/asset/v1"
+	"github.com/buildbarn/bb-asset-hub/pkg/storage"
 	"github.com/buildbarn/bb-storage/pkg/blobstore"
+	"github.com/buildbarn/bb-storage/pkg/blobstore/buffer"
 	"github.com/buildbarn/bb-storage/pkg/digest"
-
+	"github.com/buildbarn/bb-storage/pkg/util"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -28,9 +30,53 @@ func NewAssetPushServer(blobAccess blobstore.BlobAccess, allowUpdatesForInstance
 }
 
 func (s *assetPushServer) PushBlob(ctx context.Context, req *remoteasset.PushBlobRequest) (*remoteasset.PushBlobResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "PushBlob not implemented")
+	instanceName, err := digest.NewInstanceName(req.InstanceName)
+	if err != nil {
+		return nil, util.StatusWrapf(err, "Invalid instance name %#v", req.InstanceName)
+	}
+
+	if !s.allowUpdatesForInstances[instanceName] {
+		return nil, status.Errorf(codes.PermissionDenied, "This service does not accept Blobs for instance %#v", req.InstanceName)
+	}
+
+	for _, uri := range req.Uris {
+		assetRef := storage.NewAssetReference(uri, req.Qualifiers)
+		refDigest, err := storage.AssetReferenceToDigest(&assetRef, instanceName)
+		if err != nil {
+			return nil, err
+		}
+
+		err = s.blobAccess.Put(ctx, refDigest, buffer.NewProtoBufferFromProto(req.BlobDigest, buffer.UserProvided))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &remoteasset.PushBlobResponse{}, nil
 }
 
 func (s *assetPushServer) PushDirectory(ctx context.Context, req *remoteasset.PushDirectoryRequest) (*remoteasset.PushDirectoryResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "PushDirectory not implemented")
+	instanceName, err := digest.NewInstanceName(req.InstanceName)
+	if err != nil {
+		return nil, util.StatusWrapf(err, "Invalid instance name %#v", req.InstanceName)
+	}
+
+	if !s.allowUpdatesForInstances[instanceName] {
+		return nil, status.Errorf(codes.PermissionDenied, "This service does not accept Directories for instance %#v", req.InstanceName)
+	}
+
+	for _, uri := range req.Uris {
+		assetRef := storage.NewAssetReference(uri, req.Qualifiers)
+		refDigest, err := storage.AssetReferenceToDigest(&assetRef, instanceName)
+		if err != nil {
+			return nil, err
+		}
+
+		err = s.blobAccess.Put(ctx, refDigest, buffer.NewProtoBufferFromProto(req.RootDirectoryDigest, buffer.UserProvided))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &remoteasset.PushDirectoryResponse{}, nil
 }
