edcd9ee9ccde9ea3b215bf21e0f6dddc7b3c8e18
Thomas Coldrick <thomas.coldrick@codethink.co.uk>
2020-08-05 11:32:14 +0100

    Add abstractions for dealing with AssetReference
    Allows easier use of the AssetReference in the servers.

---
+++ pkg/storage/qualifier_container.go
@@ -0,0 +1,25 @@
+package storage
+
+import remoteasset "github.com/bazelbuild/remote-apis/build/bazel/remote/asset/v1"
+
+type qualifierContainer []*remoteasset.Qualifier
+
+func (q qualifierContainer) Len() int {
+	return len(q)
+}
+
+func (q qualifierContainer) Less(i, j int) bool {
+	return q[i].Name < q[j].Name || (q[i].Name == q[j].Name && q[i].Value < q[j].Value)
+}
+
+func (q qualifierContainer) Swap(i, j int) {
+	q[i], q[j] = q[j], q[i]
+}
+
+func (q qualifierContainer) toArray() []*remoteasset.Qualifier {
+	var out []*remoteasset.Qualifier
+	for i := 0; i < len(q); i++ {
+		out[i] = q[i]
+	}
+	return out
+}
