d101db7cc8f5dbea593138052af5d27c8a1060a8
Josh Smith <josh.smith@codethink.co.uk>
2020-08-06 16:26:47 +0100

    Add ValidatingFetcher
    This fetcher validates the requests for FetchBlob
    and FetchDirectory to ensure they meet common validity
    criteria.

---
+++ pkg/fetch/validating_fetcher.go
@@ -0,0 +1,35 @@
+package fetch
+
+import (
+	"context"
+	remoteasset "github.com/bazelbuild/remote-apis/build/bazel/remote/asset/v1"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type validatingFetcher struct {
+	fetcher remoteasset.FetchServer
+}
+
+// NewAssetFetchServer creates a blank Fetcher with both FetchBlob
+// and FetchDirectory unimplemented
+func NewValidatingFetcher(fetcher remoteasset.FetchServer) remoteasset.FetchServer {
+	return &validatingFetcher{
+		fetcher: fetcher,
+	}
+}
+
+func (vf *validatingFetcher) FetchBlob(ctx context.Context, req *remoteasset.FetchBlobRequest) (*remoteasset.FetchBlobResponse, error) {
+	if len(req.Uris) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "FetchBlob does not support requests without any URIs specified.")
+	}
+	return vf.fetcher.FetchBlob(ctx, req)
+}
+
+func (vf *validatingFetcher) FetchDirectory(ctx context.Context, req *remoteasset.FetchDirectoryRequest) (*remoteasset.FetchDirectoryResponse, error) {
+	if len(req.Uris) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "FetchDirectory does not support requests without any URIs specified.")
+	}
+	return vf.fetcher.FetchDirectory(ctx, req)
+}
