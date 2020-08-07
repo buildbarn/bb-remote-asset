f487a5b9b8e67341fba0432a6dfe00391829079f
Thomas Coldrick <thomas.coldrick@codethink.co.uk>
2020-08-07 11:20:52 +0100

    Add support for expire_at and oldest_content
    This adds some handling of timestamps to make sure that we obey the
    expire_at and oldest_content_accepted parts of the spec.

--- pkg/fetch/caching_fetcher.go
+++ pkg/fetch/caching_fetcher.go
@@ -2,10 +2,13 @@ package fetch
 
 import (
 	"context"
+	"time"
 
 	"github.com/buildbarn/bb-asset-hub/pkg/storage"
 	bb_digest "github.com/buildbarn/bb-storage/pkg/digest"
 	"github.com/buildbarn/bb-storage/pkg/util"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/timestamp"
 
 	remoteasset "github.com/bazelbuild/remote-apis/build/bazel/remote/asset/v1"
 
@@ -22,8 +25,8 @@ type cachingFetcher struct {
 // blob remotely multiple times
 func NewCachingFetcher(fetcher remoteasset.FetchServer, assetStore *storage.AssetStore) remoteasset.FetchServer {
 	return &cachingFetcher{
-		fetcher: 	        fetcher,
-		assetStore: 		assetStore,
+		fetcher:    fetcher,
+		assetStore: assetStore,
 	}
 }
 
@@ -33,12 +36,36 @@ func (cf *cachingFetcher) FetchBlob(ctx context.Context, req *remoteasset.FetchB
 		return nil, util.StatusWrapf(err, "Invalid instance name %#v", req.InstanceName)
 	}
 
+	var oldestContentAccepted time.Time = time.Unix(0, 0)
+	if req.OldestContentAccepted != nil {
+		oldestContentAccepted, err = ptypes.Timestamp(req.OldestContentAccepted)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	// Check assetStore
 	for _, uri := range req.Uris {
 		assetRef := storage.NewAssetReference(uri, req.Qualifiers)
 		assetData, err := cf.assetStore.Get(ctx, assetRef, instanceName)
 		if err != nil {
 			continue
+		}
+
+		// Check whether the asset has expired, making sure ExpireAt was set
+		if assetData.ExpireAt != nil {
+			expireTime, err := ptypes.Timestamp(assetData.ExpireAt)
+			if err != nil || (expireTime.Before(time.Now()) && !expireTime.Equal(time.Unix(0, 0))) {
+				continue
+			}
+		}
+
+		// Check that content is newer than the oldest accepted by the request
+		if oldestContentAccepted != time.Unix(0, 0) {
+			updateTime, err := ptypes.Timestamp(assetData.LastUpdated)
+			if err != nil || updateTime.Before(oldestContentAccepted) {
+				continue
+			}
 		}
 
 		// Successful retrieval from the asset reference cache
@@ -59,7 +86,7 @@ func (cf *cachingFetcher) FetchBlob(ctx context.Context, req *remoteasset.FetchB
 
 	// Cache fetched blob
 	assetRef := storage.NewAssetReference(response.Uri, response.Qualifiers)
-	assetData := storage.NewAsset(response.BlobDigest)
+	assetData := storage.NewAsset(response.BlobDigest, getDefaultTimestamp())
 	err = cf.assetStore.Put(ctx, assetRef, assetData, instanceName)
 	if err != nil {
 		return response, err
@@ -70,6 +97,17 @@ func (cf *cachingFetcher) FetchBlob(ctx context.Context, req *remoteasset.FetchB
 
 func (cf *cachingFetcher) FetchDirectory(ctx context.Context, req *remoteasset.FetchDirectoryRequest) (*remoteasset.FetchDirectoryResponse, error) {
 	instanceName, err := bb_digest.NewInstanceName(req.InstanceName)
+	if err != nil {
+		return nil, err
+	}
+
+	var oldestContentAccepted time.Time = time.Unix(0, 0)
+	if req.OldestContentAccepted != nil {
+		oldestContentAccepted, err = ptypes.Timestamp(req.OldestContentAccepted)
+		if err != nil {
+			return nil, err
+		}
+	}
 
 	// Check refStore
 	for _, uri := range req.Uris {
@@ -79,11 +117,27 @@ func (cf *cachingFetcher) FetchDirectory(ctx context.Context, req *remoteasset.F
 			continue
 		}
 
+		// Check whether the asset has expired, making sure ExpireAt was set
+		if assetData.ExpireAt != nil {
+			expireTime, err := ptypes.Timestamp(assetData.ExpireAt)
+			if err != nil || (expireTime.Before(time.Now()) && !expireTime.Equal(time.Unix(0, 0))) {
+				continue
+			}
+		}
+
+		// Check that content is newer than the oldest accepted by the request
+		if oldestContentAccepted != time.Unix(0, 0) {
+			updateTime, err := ptypes.Timestamp(assetData.LastUpdated)
+			if err != nil || updateTime.Before(oldestContentAccepted) {
+				continue
+			}
+		}
+
 		// Successful retrieval from the asset reference cache
 		return &remoteasset.FetchDirectoryResponse{
-			Status: 			 status.New(codes.OK, "Directory fetched successfully from asset cache").Proto(),
-			Uri: 				 uri,
-			Qualifiers: 		 req.Qualifiers,
+			Status:              status.New(codes.OK, "Directory fetched successfully from asset cache").Proto(),
+			Uri:                 uri,
+			Qualifiers:          req.Qualifiers,
 			RootDirectoryDigest: assetData.Digest,
 		}, nil
 	}
@@ -97,11 +151,16 @@ func (cf *cachingFetcher) FetchDirectory(ctx context.Context, req *remoteasset.F
 
 	// Cache fetched blob
 	assetRef := storage.NewAssetReference(response.Uri, response.Qualifiers)
-	assetData := storage.NewAsset(response.RootDirectoryDigest)
+	assetData := storage.NewAsset(response.RootDirectoryDigest, getDefaultTimestamp())
 	err = cf.assetStore.Put(ctx, assetRef, assetData, instanceName)
 	if err != nil {
 		return response, err
 	}
 
 	return response, nil
+}
+
+func getDefaultTimestamp() *timestamp.Timestamp {
+	ts, _ := ptypes.TimestampProto(time.Unix(0, 0))
+	return ts
 }
--- pkg/storage/asset.go
+++ pkg/storage/asset.go
@@ -3,11 +3,15 @@ package storage
 import (
 	remoteexecution "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
 	"github.com/buildbarn/bb-asset-hub/pkg/proto/asset"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/timestamp"
 )
 
 // NewAsset creates a new Asset from request data.
-func NewAsset(digest *remoteexecution.Digest) *asset.Asset {
+func NewAsset(digest *remoteexecution.Digest, expireAt *timestamp.Timestamp) *asset.Asset {
 	return &asset.Asset{
-		Digest: digest,
+		Digest:      digest,
+		ExpireAt:    expireAt,
+		LastUpdated: ptypes.TimestampNow(),
 	}
 }
