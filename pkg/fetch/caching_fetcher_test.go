27213d93d0c1359af0f9cf675ea9d9a664d8b016
Thomas Coldrick <thomas.coldrick@codethink.co.uk>
2020-08-10 16:08:35 +0100

    cachingfetcher: Add tests for expiry, old content
    Also fixes a bug in which we were trying to cache failed responses.

--- pkg/fetch/caching_fetcher_test.go
+++ pkg/fetch/caching_fetcher_test.go
@@ -3,21 +3,22 @@ package fetch_test
 import (
 	"context"
 	"testing"
-
-	"github.com/buildbarn/bb-asset-hub/internal/mock"
-	"github.com/buildbarn/bb-asset-hub/pkg/fetch"
-	"github.com/buildbarn/bb-asset-hub/pkg/storage"
-	"github.com/buildbarn/bb-asset-hub/pkg/proto/asset"
-
-	"github.com/buildbarn/bb-storage/pkg/blobstore/buffer"
-	bb_digest "github.com/buildbarn/bb-storage/pkg/digest"
+	"time"
 
 	remoteasset "github.com/bazelbuild/remote-apis/build/bazel/remote/asset/v1"
 	remoteexecution "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
-
-	"github.com/golang/protobuf/proto"
-	"github.com/stretchr/testify/require"
+	"github.com/buildbarn/bb-asset-hub/internal/mock"
+	"github.com/buildbarn/bb-asset-hub/pkg/fetch"
+	"github.com/buildbarn/bb-asset-hub/pkg/proto/asset"
+	"github.com/buildbarn/bb-asset-hub/pkg/storage"
+	"github.com/buildbarn/bb-storage/pkg/blobstore/buffer"
+	"github.com/buildbarn/bb-storage/pkg/digest"
+	bb_digest "github.com/buildbarn/bb-storage/pkg/digest"
 	"github.com/golang/mock/gomock"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/stretchr/testify/require"
+	protostatus "google.golang.org/genproto/googleapis/rpc/status"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -45,8 +46,8 @@ func TestFetchBlobCaching(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		backendGetCall := backend.EXPECT().Get(ctx, refDigest).Return(buffer.NewBufferFromError(status.Error(codes.NotFound, "Blob not found")))
 		fetchBlobCall := mockFetcher.EXPECT().FetchBlob(ctx, request).Return(&remoteasset.FetchBlobResponse{
-			Status: status.New(codes.OK, "Success!").Proto(),
-			Uri: uri,
+			Status:     status.New(codes.OK, "Success!").Proto(),
+			Uri:        uri,
 			BlobDigest: blobDigest,
 		}, nil).After(backendGetCall)
 		backend.EXPECT().Put(ctx, refDigest, gomock.Any()).DoAndReturn(
@@ -100,8 +101,8 @@ func TestFetchDirectoryCaching(t *testing.T) {
 	t.Run("Success", func(t *testing.T) {
 		backendGetCall := backend.EXPECT().Get(ctx, refDigest).Return(buffer.NewBufferFromError(status.Error(codes.NotFound, "Directory not found")))
 		fetchDirectoryCall := mockFetcher.EXPECT().FetchDirectory(ctx, request).Return(&remoteasset.FetchDirectoryResponse{
-			Status: status.New(codes.OK, "Success!").Proto(),
-			Uri: uri,
+			Status:              status.New(codes.OK, "Success!").Proto(),
+			Uri:                 uri,
 			RootDirectoryDigest: dirDigest,
 		}, nil).After(backendGetCall)
 		backend.EXPECT().Put(ctx, refDigest, gomock.Any()).DoAndReturn(
@@ -130,4 +131,74 @@ func TestFetchDirectoryCaching(t *testing.T) {
 		require.Nil(t, err)
 		require.Equal(t, response.Status.Code, int32(codes.OK))
 	})
+}
+
+func TestCachingFetcherExpiry(t *testing.T) {
+	ctrl, ctx := gomock.WithContext(context.Background(), t)
+
+	instanceName, err := digest.NewInstanceName("foo")
+	require.NoError(t, err)
+
+	uri := "https://example.com/example.tar.gz"
+	request := &remoteasset.FetchBlobRequest{
+		InstanceName: "foo",
+		Uris:         []string{uri},
+	}
+	refDigest, err := storage.AssetReferenceToDigest(storage.NewAssetReference(uri, []*remoteasset.Qualifier{}), instanceName)
+
+	backend := mock.NewMockBlobAccess(ctrl)
+	buf := buffer.NewProtoBufferFromProto(&asset.Asset{
+		Digest: &remoteexecution.Digest{
+			Hash:      "d1bc8d3ba4afc7e109612cb73acbdddac052c93025aa1f82942edabb7deb82a1",
+			SizeBytes: 121,
+		},
+		ExpireAt:    ptypes.TimestampNow(),
+		LastUpdated: ptypes.TimestampNow(),
+	}, buffer.UserProvided)
+	backend.EXPECT().Get(ctx, refDigest).Return(buf)
+	assetStore := storage.NewAssetStore(backend, 16*1024*1024)
+	baseFetcher := fetch.NewErrorFetcher(&protostatus.Status{
+		Code:    5,
+		Message: "Not found",
+	})
+	cacheFetcher := fetch.NewCachingFetcher(baseFetcher, assetStore)
+
+	_, err = cacheFetcher.FetchBlob(ctx, request)
+	require.Equal(t, status.ErrorProto(&protostatus.Status{Code: 5, Message: "Not found"}), err)
+}
+
+func TestCachingFetcherOldestContentAccepted(t *testing.T) {
+	ctrl, ctx := gomock.WithContext(context.Background(), t)
+
+	instanceName, err := digest.NewInstanceName("bar")
+	require.NoError(t, err)
+
+	uri := "https://example.com/exampleblob.zip"
+	request := &remoteasset.FetchBlobRequest{
+		InstanceName:          "bar",
+		Uris:                  []string{uri},
+		OldestContentAccepted: ptypes.TimestampNow(),
+	}
+	refDigest, err := storage.AssetReferenceToDigest(storage.NewAssetReference(uri, []*remoteasset.Qualifier{}), instanceName)
+
+	backend := mock.NewMockBlobAccess(ctrl)
+	ts, err := ptypes.TimestampProto(time.Unix(1, 1))
+	require.NoError(t, err)
+	buf := buffer.NewProtoBufferFromProto(&asset.Asset{
+		Digest: &remoteexecution.Digest{
+			Hash:      "ad84ffc44bab3f84fc3396b4678c1fd39770fa373c3f14eedc5d60e648067960",
+			SizeBytes: 234,
+		},
+		LastUpdated: ts,
+	}, buffer.UserProvided)
+	backend.EXPECT().Get(ctx, refDigest).Return(buf)
+	assetStore := storage.NewAssetStore(backend, 16*1024*1024)
+	baseFetcher := fetch.NewErrorFetcher(&protostatus.Status{
+		Code:    5,
+		Message: "Not found",
+	})
+	cacheFetcher := fetch.NewCachingFetcher(baseFetcher, assetStore)
+
+	_, err = cacheFetcher.FetchBlob(ctx, request)
+	require.Equal(t, status.ErrorProto(&protostatus.Status{Code: 5, Message: "Not found"}), err)
 }
