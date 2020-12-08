ecba74ffd7796a067708ba74d1c04be1c84f6f89
Scott Clarke <scott.clarke@codethink.co.uk>
2020-12-08 12:09:45 +0000

    Add comments and update README
    After review it was apparent that some comments were needed to clarify
    parts of the implementation.
    
    Also the README has been updated with the config changes

--- pkg/configuration/new_asset_store.go
+++ pkg/configuration/new_asset_store.go
@@ -15,17 +15,17 @@ func NewAssetStoreAndCASFromConfiguration(configuration *pb.AssetCacheConfigurat
 	var assetStore storage.AssetStore
 	var contentAddressableStorage blobstore.BlobAccess
 	switch backend := configuration.Backend.(type) {
-	case *pb.AssetCacheConfiguration_LocalBlobAccess:
+	case *pb.AssetCacheConfiguration_BlobAccess:
 		assetBlobAccessCreator := asset_configuration.NewAssetBlobAccessCreator(grpcClientFactory, maximumMessageSizeBytes)
 
 		assetBlobAccess, err := blobstore_configuration.NewBlobAccessFromConfiguration(
-			backend.LocalBlobAccess.AssetStore,
+			backend.BlobAccess.AssetStore,
 			assetBlobAccessCreator)
 		if err != nil {
 			return nil, nil, err
 		}
 		assetStore = storage.NewBlobAccessAssetStore(assetBlobAccess.BlobAccess, maximumMessageSizeBytes)
-		contentAddressableStorageInfo, err := blobstore_configuration.NewBlobAccessFromConfiguration(backend.LocalBlobAccess.ContentAddressableStorage, blobstore_configuration.NewCASBlobAccessCreator(grpcClientFactory, maximumMessageSizeBytes))
+		contentAddressableStorageInfo, err := blobstore_configuration.NewBlobAccessFromConfiguration(backend.BlobAccess.ContentAddressableStorage, blobstore_configuration.NewCASBlobAccessCreator(grpcClientFactory, maximumMessageSizeBytes))
 		if err != nil {
 			return nil, nil, err
 		}
